e3d9ba4963bc0f47a70ee5717e0698bb289ded39
adguerrero <adguerrero@syc.com.co>
2022-07-08 08:42:51 -0500

    comienzo a verificar parches

--- valmodel.py
+++ valmodel.py
@@ -29,6 +29,7 @@ transform = transforms.Compose([
             transforms.Grayscale(num_output_channels=1),
             transforms.ToTensor()
             ])
+            
 val_dataset = data.MyDataset(DF(args.baseroot,'val'), transform, use_cache=False)
 valloader = DataLoader(val_dataset, batch_size=batch_size)
 
@@ -44,32 +45,22 @@ checkpoint = torch.load(args.model)
 model.load_state_dict(checkpoint['state_dict'])
 criterion = nn.MSELoss()
 
-d_transform = transforms.Compose([
-            transforms.ToPILImage()
-            ])
 with torch.no_grad():
         model.eval()
         for k, pair in enumerate(valloader):
             start_time = time.time()
             imgC, imgN = pair[0].to(device), pair[1].to(device)
 
-            d = 40 # size patch
-            w = imgN.size(2)
-            h = imgN.size(3)
-            reconT = torch.zeros(imgN.size()).to(device)
-            grid = product(range(0, h-h%d, d), range(0, w-w%d, d))
-            for i, j in grid:
-                box = (j, i, j+d, i+d)
-                recon = model(transform(d_transform(imgN.squeeze()).crop(box)).to(device).unsqueeze(0),device)
-                reconT[:,:,i:i+d,j:j+d] = recon
-            loss = criterion(reconT,imgC)
+            recon = model(imgN,device)
+            #reconT[:,:,i:i+d,j:j+d] = recon
+            loss = criterion(recon,imgC)
             recon_time = time.time() - start_time
             
             # Peak Signal to Noise Ratio
             psnr = 10 * torch.log10(torch.tensor([1]) / loss.item())
-            reconT = reconT.squeeze().cpu().numpy()
+            recon = recon.squeeze().cpu().numpy()
             imgN = imgN.squeeze().cpu().numpy()
-            result = cv2.hconcat((imgN, reconT))
+            result = cv2.hconcat((imgN, recon))
 
             cv2.imwrite(args.output_path + 'img_' + str(k) + '.jpg', (result*255).astype('uint8'))
             print('Loss: {:.2E}, PSNR: {:,.2f}, time: {:,.2f}'.format(Decimal(loss.item()), psnr.item(), recon_time))
