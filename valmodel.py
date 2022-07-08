af239cf203725e81e180ba0d87ee459781165d8b
adguerrero <adguerrero@syc.com.co>
2022-07-08 08:35:05 -0500

    nuevo modelo

--- valmodel.py
+++ valmodel.py
@@ -49,7 +49,7 @@ d_transform = transforms.Compose([
             ])
 with torch.no_grad():
         model.eval()
-        for i, pair in enumerate(valloader):
+        for k, pair in enumerate(valloader):
             start_time = time.time()
             imgC, imgN = pair[0].to(device), pair[1].to(device)
 
@@ -60,16 +60,16 @@ with torch.no_grad():
             grid = product(range(0, h-h%d, d), range(0, w-w%d, d))
             for i, j in grid:
                 box = (j, i, j+d, i+d)
-                recon = model(transform(d_transform(imgN.imgN.squeeze()).crop(box)).unsqueeze(0),device)
-                reconT[:,:,j:j+d,i:i+d] = recon
+                recon = model(transform(d_transform(imgN.squeeze()).crop(box)).to(device).unsqueeze(0),device)
+                reconT[:,:,i:i+d,j:j+d] = recon
             loss = criterion(reconT,imgC)
             recon_time = time.time() - start_time
             
             # Peak Signal to Noise Ratio
             psnr = 10 * torch.log10(torch.tensor([1]) / loss.item())
-            reconT = reconT.squeeze().numpy()
-            imgN = imgN.squeeze().numpy()
+            reconT = reconT.squeeze().cpu().numpy()
+            imgN = imgN.squeeze().cpu().numpy()
             result = cv2.hconcat((imgN, reconT))
 
-            cv2.imwrite(args.output_path + 'img_' + str(i) + '.jpg', (result*255).astype('uint8'))
-            print('Loss: {:.2E}, PSNR: {:,.2f}, time: {:,.2f}'.format(Decimal(loss.item()), psnr, recon_time))
+            cv2.imwrite(args.output_path + 'img_' + str(k) + '.jpg', (result*255).astype('uint8'))
+            print('Loss: {:.2E}, PSNR: {:,.2f}, time: {:,.2f}'.format(Decimal(loss.item()), psnr.item(), recon_time))
